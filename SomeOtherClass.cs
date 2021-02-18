87adc950b3dc7218e6439ed056581ad4e2000490
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-02-17 21:42:22 -0700

    Method Overloading

---
+++ SomeOtherClass.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class SomeOtherClass : MonoBehavior
+{
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        // The specific Add method called will depend on
+        // the arguments passed in
+        myClass.Add(1, 2);
+        myClass.Add("Hello, ", "World!"); // <- I'm getting a tattoo of this saying in May :3
+    }
+}
