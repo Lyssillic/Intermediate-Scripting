76cdfacde44e4e92b0e4928b9b992cc6f934293c
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-02-18 22:25:32 -0700

    Inheritance with fruit

---
+++ Apple.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+// This is the derived class which is also known as a Child
+public class Apple : Fruit
+{
+    // This is the first constructor for the Apple class
+    // It calls the parent constructor immediately, before it runs
+    public Apple()
+    {
+        // Notice how Apple has access to the public variable
+        // color, which is a part of the parent Fruit
+        color = "red";
+        Debug.Log("1st Apple Constructor Called");
+    }
+
+    // Specifies which parent constructor will be called
+    // using the "base" keyword.
+    public Apple(string newColor) : base(newColor)
+    {
+        // Notice how this constructor doesn't set the color
+        // since the base constructor sets the color that
+        // is passed as an argument
+        Debug.Log("2nd Apple Constructor Called");
+    }
+}
